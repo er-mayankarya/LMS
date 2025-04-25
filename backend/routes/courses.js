06fc1af6784b812517d130d4d7b9d55cacae166c
Mayank Arya <ermayankarya7@gmail.com>
2025-04-25 08:34:16 +0530

    Coursess routes added

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,10 +1,29 @@
 import { Router } from "express";
+import { courseModel, purchaseModel } from "../db.js";
+import { userMiddleware } from "../middlewares/userMiddleware.js";
 export const courseRouter = Router();
 
-courseRouter.post("/purchase" , (req , res) => {
+courseRouter.post("/purchase" ,userMiddleware , async (req , res) => {
+
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+
+    await purchaseModel.create({
+        userId ,
+        courseId
+    })
+    res.json({
+        message : "You have sucessfully bought the course"
+    })
 
 });
 
-courseRouter.get("/preview" , (req , res) => {
+courseRouter.get("/preview" , async (req , res) => {
+
+    const courses = await courseModel.find({});
+
+    res.json({
+        courses
+    })
 
 });
